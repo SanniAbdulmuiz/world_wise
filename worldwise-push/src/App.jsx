22669c2410aa2fe0aacb8def13786cc1b9d85b80
Abdulmuiz <ayomideabdulmuiz565@gmail.com>
2024-09-24 02:00:20 +0100

    Add files via upload

--- worldwise-push/src/App.jsx
+++ worldwise-push/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import AppLayout from "./pages/AppLayout";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- worldwise-push/starter/pages/Homepage.jsx
+++ worldwise-push/starter/pages/Homepage.jsx
@@ -1,5 +1,4 @@
 import styles from "./Homepage.module.css";
-import { Link } from "react-router-dom";
 
 export default function Homepage() {
   return (
@@ -15,9 +14,6 @@ export default function Homepage() {
           of. Never forget your wonderful experiences, and show your friends how
           you have wandered the world.
         </h2>
-        <Link to="/app" className="cta">
-          Start tracking now
-        </Link>
       </section>
     </main>
   );
